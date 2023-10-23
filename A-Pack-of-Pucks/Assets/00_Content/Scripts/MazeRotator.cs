b5e557e84d36e822da58b96798ab5f5decab1f7c
DreamingJohnny <johnnydreaming@gmail.com>
2023-10-23 20:20:59 +0200

    added mathx and improved rotation script

--- A-Pack-of-Pucks/Assets/00_Content/Scripts/MazeRotator.cs
+++ A-Pack-of-Pucks/Assets/00_Content/Scripts/MazeRotator.cs
@@ -4,57 +4,55 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.Mathematics;
+using static Unity.Mathematics.mathx;
 
 public class MazeRotator : MonoBehaviour {
-	//So, I need to get mouse position, and "up" on the maze, and then distance in between, as angle, and rotate based on that.
-	private Quaternion rotation;
 
-	//These should probably become degrees in the end...
-	private Vector2 mousePosition;
-	private Vector2 startMousePosition;
-	private Vector2 currentMousePosition;
+	[SerializeField] private Transform maze;
+	[SerializeField] private float m_rotSpeed;
 
+	private float m_rot;
 
-	private bool shouldRotate;
+	private float m_beginRot;
+	private float m_beginTheta;
 
-	void Start() {
+	private void Update() {
+		Vector2 wheelPos = maze.position; //you may need to do some coordinate space conversion on this later
+		Vector2 mousePos = Input.mousePosition;
 
-	}
+		Vector2 localMousePos = mousePos - wheelPos;
 
-	void Update() {
-	
-	}
+		float theta = Mathf.Atan2(localMousePos.y, localMousePos.x);
 
-	private void FixedUpdate() {
-		if (shouldRotate) {
-			RotateMaze();
+		if (Input.GetMouseButtonDown(0)) {
+			m_beginTheta = theta;
+			m_beginRot = m_rot;
+		}
+		if (Input.GetMouseButton(0)) {
+			float delta = theta - m_beginTheta;
+			float rotTarget = m_beginRot + delta;
+			m_rot = RotateTowards(m_rot, rotTarget, m_rotSpeed * Mathf.Deg2Rad * Time.deltaTime);
+
+			maze.rotation = Quaternion.Euler(0, 0, m_rot * Mathf.Rad2Deg);
 		}
 	}
 
-	private void RotateMaze() {
-		currentMousePosition = Input.mousePosition;
+	private static float RotateTowards(float current, float target, float maxTurn) {
+		float diff = SignedRadialDist(current, target);
+		float diffClamped = Mathf.Clamp(diff, -maxTurn, maxTurn);
 
-		Quaternion quaternion = 
-			//Mathf.LerpAngle
-
-		transform.Rotate();
-
-		Debug.Log(mousePosition);
-
-		//So, I want to take mouse position on started, and on performed, and take the difference, and then I want to apply the difference,
-		//So,
-
+		return WrapRot(current + diffClamped);
 	}
 
-	public void OnRotate(InputAction.CallbackContext context) {
+	private static float SignedRadialDist(float current, float target) {
+		float diff0 = target - current;
+		float diff1 = (mathx.TAU - Mathf.Abs(diff0)) * Mathf.Sign(-diff0);
+		float diffMin = Mathf.Abs(diff0) < Mathf.Abs(diff1) ? diff0 : diff1;
 
-		if(context.started) {
-			shouldRotate = true;
-			startMousePosition = Input.mousePosition;
-		}
-		else if (context.canceled) {
-			shouldRotate = false;
-			startMousePosition = Vector2.zero;
-		}		
+		return diffMin;
 	}
+
+	private static float WrapRot(float angle) =>
+		mathx.mod(angle + Mathf.PI, mathx.TAU) - Mathf.PI;
 }
